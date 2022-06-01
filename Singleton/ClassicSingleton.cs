4b71afe6731eb0fa732ab01890439e6e224626a8
Karmah24 <kartikmahajan42116@gmail.com>
2022-06-01 16:32:08 +0530

    Singleton Pattern

---
+++ Singleton/ClassicSingleton.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    // not thread safe
+    public class ClassicSingleton
+    {
+        private static ClassicSingleton uniqueInstance = null;
+        private ClassicSingleton() {}
+        public ClassicSingleton getInstance()
+        {
+            if (uniqueInstance == null) uniqueInstance = new ClassicSingleton();
+            return uniqueInstance;
+        }
+        public void getDescription()
+        {
+            System.Console.WriteLine("Classic Singleton");
+        }
+    }
+}
