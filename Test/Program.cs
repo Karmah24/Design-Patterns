b6664d7b622ccb3a894a6b5e434e82401cefb1d3
Karmah24 <kartikmahajan42116@gmail.com>
2022-06-23 10:12:06 +0530

    Singleton

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿class Test
+{
+    static void Main(string[] args)
+    {
+        SortedDictionary<int, string> dict;
+        dict = new SortedDictionary<int, string>();
+
+        dict[15] = "fifteen";
+        dict[5] = "five";
+        dict[20] = "twenty";
+
+        var enumartor = dict.GetEnumerator();
+        enumartor.MoveNext();
+        var x = enumartor.Current;
+        System.Console.WriteLine(x);
+    }
+}
